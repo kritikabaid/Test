d6c0664c8ff35f4c407ea44678ad7a1253cda9f0
kritikabaid <46103242+kritikabaid@users.noreply.github.com>
2019-01-08 16:52:56 +0530

    Update Main.java
    changeddddddddddddddd

--- example.java.helloworld-master/HelloWorld/Main.java
+++ example.java.helloworld-master/HelloWorld/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello  Github world 1234!!!!!!");
+        System.out.println("Hello  DEVOPS  world 1234!!!!!!");
     }
 }
 
